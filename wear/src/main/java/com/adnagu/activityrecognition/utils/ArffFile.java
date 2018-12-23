da4844844e259ea74dc830bf09952f381470ed53
Ramazan VAPURCU <wmrmzn@gmail.com>
2018-12-24 00:18:08 +0300

    Updated ArffFile class.

--- wear/src/main/java/com/adnagu/activityrecognition/utils/ArffFile.java
+++ wear/src/main/java/com/adnagu/activityrecognition/utils/ArffFile.java
@@ -1,7 +1,16 @@
 package com.adnagu.activityrecognition.utils;
 
+import android.content.Context;
+import android.hardware.Sensor;
+
+import com.adnagu.activityrecognition.database.entity.SensorEntity;
 import com.adnagu.activityrecognition.database.entity.SensorRecordEntity;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,18 +23,76 @@ public class ArffFile {
 
     public static final String  FILE_NAME = "ActivityRecords.arff";
     public static final int     NANO_SECONDS = 1000000000;
+    public static final int[]   SENSOR_TYPES = {
+            Sensor.TYPE_ACCELEROMETER,
+            Sensor.TYPE_MAGNETIC_FIELD,
+            Sensor.TYPE_GYROSCOPE,
+            Sensor.TYPE_GRAVITY,
+            Sensor.TYPE_LINEAR_ACCELERATION,
+            /*Sensor.TYPE_ROTATION_VECTOR,
+            Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED,
+            Sensor.TYPE_GAME_ROTATION_VECTOR,
+            Sensor.TYPE_GYROSCOPE_UNCALIBRATED,
+            Sensor.TYPE_SIGNIFICANT_MOTION,
+            Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR*/
+    };
+    public static final char[]  VALUE_TYPES = {
+            'x',
+            'y',
+            'z',
+            'a',
+            'b',
+            'c',
+            'd',
+            'e'
+    };
 
-    public static void saveAsArff(List<SensorRecordEntity> sensorRecords, int windowLength) {
+    public static void saveAsArff(Context context, List<SensorRecordEntity> sensorRecords, int windowLength) {
+        try {
+            // Prepare file
+            OutputStreamWriter writer = new OutputStreamWriter(
+                    context.openFileOutput(
+                            FILE_NAME,
+                            Context.MODE_PRIVATE
+                    )
+            );
+            writer.write("@relation activity_recognition\n\n");
 
-        int index;
-        long lastTimestamp;
+            // Write attributes
+            writer.write("@attribute activity {0,1,2,3,4,5,6}\n");
+            writer.write("@attribute sequence sensor_records");
 
-        for (index = 0; index < sensorRecords.size(); index++) {
-            SensorRecordEntity sensorRecord = sensorRecords.get(index);
+            for (int sensorType : SENSOR_TYPES)
+                for (int i = 0; i < 3; i++)
+                    writer.write("\t@attribute sensor" + sensorType +"_" + VALUE_TYPES[i] + " numeric");
 
-            lastTimestamp = sensorRecord.getTimestamp();
+            writer.write("@end sequence");
+
+            // Write records
+            writer.write("@data\n");
+
+            int index;
+            long lastTimestamp;
+
+            for (index = 0; index < sensorRecords.size(); index++) {
+                SensorRecordEntity sensorRecord = sensorRecords.get(index);
+                if (isValidSensorType(sensorRecord.getId())) {
+                    lastTimestamp = sensorRecord.getTimestamp();
+
+                }
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    public static boolean isValidSensorType(int id) {
+        for (int SENSOR_TYPE : SENSOR_TYPES)
+            if (id == SENSOR_TYPE)
+                return true;
+        return false;
     }
 
 }
